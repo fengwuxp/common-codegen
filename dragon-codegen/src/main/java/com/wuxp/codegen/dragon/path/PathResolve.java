5b6443ffc8aad3402c6eac0673e3e7dd02e67fe3
风 <1109695647@qq.com>
2019-04-20 20:46:07 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- dragon-codegen/src/main/java/com/wuxp/codegen/dragon/path/PathResolve.java
+++ dragon-codegen/src/main/java/com/wuxp/codegen/dragon/path/PathResolve.java
@@ -37,10 +37,7 @@ public class PathResolve {
         if (args == null || args.length <= 1) {
             return null;
         }
-        String args1 = args[1];
-        if (!args1.startsWith(".") && !args1.startsWith(File.separator)) {
-            return args1;
-        }
+
         Path basePath = Paths.get(baseDir);
 
         List<Path> paths = Arrays.stream(args)
@@ -48,13 +45,16 @@ public class PathResolve {
                 .map(path -> basePath.resolveSibling(Paths.get(path)))
                 .collect(Collectors.toList());
         if (paths.size() == 0) {
-            log.error("根路径{}，需要导入依赖的文件，导入的依赖路径", baseDir, args[0], args1);
+            log.error("根路径{}，需要导入依赖的文件，导入的依赖路径", baseDir, args[0], args[1]);
             return null;
         }
-
+        String pathArgs2 = paths.get(1).toString();
+        if (!pathArgs2.startsWith(".") && !pathArgs2.startsWith(File.separator)) {
+            return args[1];
+        }
         Path p = paths.get(0);
         for (int i = 1; i < paths.size(); i++) {
-            p = p.relativize( paths.get(i));
+            p = p.relativize(paths.get(i));
         }
         String result = p.normalize().toString();
 
