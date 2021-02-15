f240bdc0701cafdf67d74e17038a27500590542f
伍枭鹏 <1109695647@qq.com>
2021-02-15 12:28:35 +0800

    feature(loong-quick)：分离DocController用于定义OpenApi

--- loong-quick/codegen-server/src/main/java/com/wuxp/codegen/server/controller/LoongCodegenController.java
+++ loong-quick/codegen-server/src/main/java/com/wuxp/codegen/server/controller/LoongCodegenController.java
@@ -14,16 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author wuxp
  */
-@OpenAPIDefinition(
-        info = @Info(
-                title = "sdk codegen 平台",
-                version = "1.0.0",
-                description = "通过从代码管理平台（git/svn）中获取源代码进行代码生成"
-        ),
-        externalDocs = @ExternalDocumentation(description = "代码生成文档",
-                url = "https://github.com/fengwuxp/common-codegen/blob/master/docs/doc.md"
-        )
-)
 @Tag(name = "loong codegen", description = "代码生成restful接口")
 @Slf4j
 @RestController
