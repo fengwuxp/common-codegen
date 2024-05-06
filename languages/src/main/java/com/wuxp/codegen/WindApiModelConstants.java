6611ffc5b78117641e695a6d1f50cb0d6c5b4f0b
伍枭鹏 <1109695647@qq.com>
2024-05-06 18:58:46 +0800

    fix: wind java model

--- languages/src/main/java/com/wuxp/codegen/WindApiModelConstants.java
+++ languages/src/main/java/com/wuxp/codegen/WindApiModelConstants.java
@@ -1,9 +1,11 @@
 package com.wuxp.codegen;
 
 import com.wind.api.core.ApiResponse;
+import com.wind.api.core.ImmutableApiResponse;
 import com.wind.client.retrofit.query.AbstractPageQueryMap;
 import com.wind.common.query.supports.AbstractPageQuery;
 import com.wind.common.query.supports.DefaultOrderField;
+import com.wind.common.query.supports.ImmutablePagination;
 import com.wind.common.query.supports.Pagination;
 import com.wind.common.query.supports.QueryOrderType;
 import com.wind.common.query.supports.QueryType;
@@ -26,13 +28,13 @@ public final class WindApiModelConstants {
 
     private static final String TS_FEIGN_CLIENT_PACKAGE_PATH = "feign-client";
 
-    private static final String PAGINATION_GENERIC_NAME = "Pagination<T>";
+    private static final String PAGINATION_GENERIC_NAME = "ImmutablePagination<T>";
 
-    private static final String API_RESPONSE_GENERIC_NAME = "ApiResponse<T>";
+    private static final String API_RESPONSE_GENERIC_NAME = "ImmutableApiResponse<T>";
 
-    public static final JavaCodeGenClassMeta JAVA_API_RESPONSE = new JavaCodeGenClassMeta(ApiResponse.class, API_RESPONSE_GENERIC_NAME, ApiResponse.class.getName(), true);
+    public static final JavaCodeGenClassMeta JAVA_API_RESPONSE = new JavaCodeGenClassMeta(ImmutableApiResponse.class, API_RESPONSE_GENERIC_NAME, ImmutableApiResponse.class.getName(), true);
 
-    public static final JavaCodeGenClassMeta JAVA_PAGINATION = new JavaCodeGenClassMeta(Pagination.class, PAGINATION_GENERIC_NAME, Pagination.class.getName(), true);
+    public static final JavaCodeGenClassMeta JAVA_PAGINATION = new JavaCodeGenClassMeta(ImmutablePagination.class, PAGINATION_GENERIC_NAME, ImmutablePagination.class.getName(), true);
 
     public static final JavaCodeGenClassMeta JAVA_ABSTRACT_PAGE_QUERY = new JavaCodeGenClassMeta(AbstractPageQuery.class, null, AbstractPageQuery.class.getName(), true);
 
