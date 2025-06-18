f05095598cfae634e50e6ee5b358ed7a670fc024
wuxp <1109695647@qq.com>
2025-06-18 14:33:27 +0800

    build(deps): 更新项目依赖并调整模块结构
    - 移除 springdoc-openapi 相关依赖
    - 添加 swagger-annotations-jakarta 依赖
    - 更新 Swagger3Config 配置示例
    - 禁用 MavenCodegenPluginExecuteStrategyTests 和 MavenCommandInvokeUtilsTests 测试类
    - 优化 ParameterMetaFactory 注释工厂

--- examples/swagger-3/src/main/java/com/wuxp/codegen/swagger3/example/config/Swagger3Config.java
+++ examples/swagger-3/src/main/java/com/wuxp/codegen/swagger3/example/config/Swagger3Config.java
@@ -1,26 +1,14 @@
 package com.wuxp.codegen.swagger3.example.config;
 
-import io.swagger.v3.oas.models.info.Info;
+
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Swagger3Config {
 
-    //    @Bean
-//    public OpenAPI openAPI() {
-//        return new OpenAPI()
-//                .components(new Components()
-//                        .addSecuritySchemes("basicScheme", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
-//                .info(new Info()
-//                        .title("example API")
-//                        .version("1.0.0")
-//                        .license(new License()
-//                                .name("example")
-//                                .url("http://example.com")));
-//    }
     @Bean
     public GroupedOpenApi usersGroup() {
         return GroupedOpenApi.builder().group("example")
@@ -28,7 +16,7 @@ public class Swagger3Config {
                     operation.addSecurityItem(new SecurityRequirement().addList("basicScheme"));
                     return operation;
                 })
-                .addOpenApiCustomiser(openApi -> openApi.info(new Info().title("Example API").version("1.0.0")))
+                .addOpenApiCustomizer(openApi -> openApi.info(openApi.getInfo().title("Example API").version("1.0.0")))
                 .packagesToScan("com.wuxp.codegen.swagger3.example")
                 .build();
     }
